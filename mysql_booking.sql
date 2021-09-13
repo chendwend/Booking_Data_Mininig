1a6538030975e3c201860564cca9d0af6692098f
anatkira <78455947+anatkira@users.noreply.github.com>
2021-09-13 21:45:14 +0300

    Update mysql_booking.sql

--- mysql_booking.sql
+++ mysql_booking.sql
@@ -2,27 +2,24 @@ CREATE DATABASE IF NOT EXISTS booking_data;
 
 USE booking_data;
 
-CREATE TABLE location (
-  location_id INT NOT NULL AUTO_INCREMENT,
-  location VARCHAR(100) NOT NULL,
-  PRIMARY KEY (location_id)
+CREATE TABLE sub_location (
+  location_id INT NOT NULL PRIMARY KEY,
+  location VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE site_basic_info (
-site_id INT NOT NULL AUTO_INCREMENT,
-location_id INT NOT NULL,
+site_id INT NOT NULL PRIMARY KEY,
 site_name VARCHAR(100) NOT NULL,
+location_id INT NOT NULL,
 price FLOAT(2),
 max_people SMALLINT,
 rating FLOAT(2),
 reviewers_amount SMALLINT,
-PRIMARY KEY (site_id),
-FOREIGN KEY (location_id) REFERENCES location(location_id) ON DELETE CASCADE
+FOREIGN KEY (location_id) REFERENCES sub_location(location_id) ON DELETE CASCADE
 );
 
 CREATE TABLE site_more_info (
-info_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-site_id INT NOT NULL,
+site_id INT NOT NULL PRIMARY KEY,
 free_cancellation TINYINT(1),
 parking TINYINT(1),
 breakfast TINYINT(1),
@@ -31,8 +28,7 @@ FOREIGN KEY (site_id) REFERENCES site_basic_info(site_id) ON DELETE CASCADE
 );
 
 CREATE TABLE room_facilities (
-facilities_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-site_id INT NOT NULL,
+site_id INT NOT NULL PRIMARY KEY,
 kitchen TINYINT(1),
 wifi TINYINT(1),
 air_conditioning TINYINT(1),
