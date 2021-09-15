e7708510a45a656a332fef3cbcb4b8f338270223
anatkira <78455947+anatkira@users.noreply.github.com>
2021-09-15 14:04:26 +0300

    Update mysql_booking.sql

--- mysql_booking.sql
+++ mysql_booking.sql
@@ -4,34 +4,34 @@ USE booking_data;
 
 CREATE TABLE sub_location (
   location_id INT NOT NULL PRIMARY KEY,
-  location VARCHAR(100) NOT NULL
+  location VARCHAR(100) NOT NULL 
 );
 
 CREATE TABLE site_basic_info (
 site_id INT NOT NULL PRIMARY KEY,
 site_name VARCHAR(100) NOT NULL,
 location_id INT NOT NULL,
-price FLOAT(2),
-max_people SMALLINT,
-rating FLOAT(2),
-reviewers_amount SMALLINT,
+price INT DEFAULT NULL,
+max_people SMALLINT DEFAULT NULL,
+rating FLOAT(2) DEFAULT NULL,
+reviewers_amount SMALLINT DEFAULT NULL,
 FOREIGN KEY (location_id) REFERENCES sub_location(location_id) ON DELETE CASCADE
 );
 
 CREATE TABLE site_more_info (
 site_id INT NOT NULL PRIMARY KEY,
-free_cancellation TINYINT(1),
-parking TINYINT(1),
-breakfast TINYINT(1),
-pets TINYINT(1),
+free_cancellation TINYINT(1) DEFAULT NULL,
+parking TINYINT(1) DEFAULT NULL,
+breakfast TINYINT(1) DEFAULT NULL,
+pets TINYINT(1) DEFAULT NULL,
 FOREIGN KEY (site_id) REFERENCES site_basic_info(site_id) ON DELETE CASCADE
 );
 
 CREATE TABLE room_facilities (
 site_id INT NOT NULL PRIMARY KEY,
-kitchen TINYINT(1),
-wifi TINYINT(1),
-air_conditioning TINYINT(1),
+kitchen TINYINT(1) DEFAULT NULL,
+wifi TINYINT(1) DEFAULT NULL,
+air_conditioning TINYINT(1) DEFAULT NULL,
 FOREIGN KEY (site_id) REFERENCES site_basic_info(site_id) ON DELETE CASCADE
 );
 
