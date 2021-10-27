aed3a17e6e9cb09da6a168365788e54008b470c6
Anat Kira <anatkira@gmail.com>
2021-10-27 21:32:57 +0300

    simplify the DB

--- sources/from_csv_to_db.py
+++ sources/from_csv_to_db.py
@@ -3,8 +3,14 @@ import csv
 from datetime import datetime
 from utilities.config import FILE_NAME
 
+#FILE_NAME="data.csv"
 
 def insert_to_db(from_date, to_date, location):
+    """
+    The function receives dates and location and inserts it with the data from the csv file
+    to the different tables in the DB.
+    """
+
     connection = pymysql.connect(host='localhost',
                                  user='root',
                                  password='root',
@@ -18,47 +24,43 @@ def insert_to_db(from_date, to_date, location):
     # cur.execute('''ALTER DATABASE booking_data CHARACTER SET utf8''')
     # cur.execute('''USE booking_data''')
     #
-    # cur.execute('''CREATE TABLE site_location (
-    #                location_id INT AUTO_INCREMENT PRIMARY KEY,
-    #                location VARCHAR(100) NOT NULL,
-    #                sub_location VARCHAR(100) NOT NULL,
-    #                UNIQUE (location, sub_location)
-    #                                           )'''
+    # cur.execute('''CREATE TABLE location_dates (
+    #                 id INT AUTO_INCREMENT PRIMARY KEY,
+    #                 location VARCHAR(100) NOT NULL,
+    #                 sub_location VARCHAR(100) NOT NULL,
+    #                 sub_location_latitude FLOAT DEFAULT NULL,
+    #                 sub_location_longitude FLOAT DEFAULT NULL,
+    #                 site_name VARCHAR(100) NOT NULL,
+    #                 from_date DATE NOT NULL,
+    #                 to_date DATE NOT NULL,
+    #                 UNIQUE (location, sub_location, site_name, from_date, to_date)
+    #                                             )'''
     #             )
     # cur.execute('''CREATE TABLE site_info (
-    #                site_id INT AUTO_INCREMENT PRIMARY KEY,
-    #                location_id INT NOT NULL,
-    #                site_name VARCHAR(1000) NOT NULL,
-    #                max_people SMALLINT DEFAULT NULL,
-    #                rating FLOAT(2) DEFAULT NULL,
-    #                reviewers_amount SMALLINT DEFAULT NULL,
-    #                free_cancellation TINYINT(1) DEFAULT NULL,
-    #                parking TINYINT(1) DEFAULT NULL,
-    #                breakfast TINYINT(1) DEFAULT NULL,
-    #                pets TINYINT(1) DEFAULT NULL,
-    #                UNIQUE (location_id, site_name),
-    #                FOREIGN KEY (location_id) REFERENCES site_location(location_id) ON DELETE CASCADE
+    #                 site_id INT AUTO_INCREMENT PRIMARY KEY,
+    #                 location_dates_id INT NOT NULL,
+    #                 rating FLOAT(2) DEFAULT NULL,
+    #                 reviewers_amount SMALLINT DEFAULT NULL,
+    #                 free_cancellation TINYINT(1) DEFAULT NULL,
+    #                 parking TINYINT(1) DEFAULT NULL,
+    #                 breakfast TINYINT(1) DEFAULT NULL,
+    #                 pets TINYINT(1) DEFAULT NULL,
+    #                 price INT NOT NULL,
+    #                 date_time DATETIME NOT NULL,
+    #                 temperature INT DEFAULT NULL,
+    #                 feelslike INT DEFAULT NULL,
+    #                 UNIQUE (location_dates_id),
+    #                 FOREIGN KEY (location_dates_id) REFERENCES location_dates(id) ON DELETE CASCADE
     #                                         )'''
     #             )
-    # cur.execute('''CREATE TABLE price_offer (
-    #                offer_id INT AUTO_INCREMENT PRIMARY KEY,
-    #                location_id INT NOT NULL,
-    #                site_id INT NOT NULL,
-    #                price INT NOT NULL,
-    #                from_date DATE NOT NULL,
-    #                to_date DATE NOT NULL,
-    #                date_time_update DATETIME NOT NULL,
-    #                FOREIGN KEY (location_id) REFERENCES site_location(location_id) ON DELETE CASCADE,
-    #                FOREIGN KEY (site_id) REFERENCES site_info(site_id) ON DELETE CASCADE,
-    #                UNIQUE (location_id, site_id, from_date, to_date)
-    #                                          )'''
-    #             )
     # cur.execute('''CREATE TABLE facilities (
-    #                site_id INT PRIMARY KEY,
-    #                kitchen TINYINT(1) DEFAULT NULL,
-    #                wifi TINYINT(1) DEFAULT NULL,
-    #                air_conditioning TINYINT(1) DEFAULT NULL,
-    #                FOREIGN KEY (site_id) REFERENCES site_info(site_id) ON DELETE CASCADE
+    #                 site_id INT AUTO_INCREMENT PRIMARY KEY,
+    #                 location_dates_id INT NOT NULL,
+    #                 kitchen TINYINT(1) DEFAULT NULL,
+    #                 wifi TINYINT(1) DEFAULT NULL,
+    #                 air_conditioning TINYINT(1) DEFAULT NULL,
+    #                 UNIQUE (location_dates_id),
+    #                 FOREIGN KEY (location_dates_id) REFERENCES location_dates(id) ON DELETE CASCADE
     #                                         )'''
     #             )
 
@@ -69,62 +71,68 @@ def insert_to_db(from_date, to_date, location):
     for row in enumerate(reader):
         row = row[1]
 
-        insert_query_1 = '''INSERT INTO site_location (location, sub_location)
-                            SELECT * FROM (SELECT %s AS location, %s AS sub_location) AS temp
-                            WHERE NOT EXISTS(
-                            SELECT location, sub_location FROM site_location WHERE location = %s and sub_location = %s
-                            )LIMIT 1'''
-        values_1 = (location, row.get('sub location'), location, row.get('sub location'))
+        # filling the location_dates table, because we defined that
+        # (location, sub_location, site_name, from_date, to_date) are unique key, we check if this combination
+        # is already exist, and insert if it isn't.
+        insert_query_1 = '''INSERT INTO location_dates (location, sub_location, sub_location_latitude, 
+                                                        sub_location_longitude, site_name, from_date, to_date)
+                            SELECT * FROM (SELECT %s AS location, %s AS sub_location, %s AS sub_location_latitude, 
+                                           %s AS sub_location_longitude, %s AS site_name, %s AS from_date, 
+                                           %s AS to_date) AS temp
+                            WHERE NOT EXISTS(SELECT location, sub_location, site_name, from_date, to_date 
+                                             FROM location_dates 
+                                             WHERE location = %s and sub_location = %s and site_name = %s and
+                                                   from_date = %s and to_date = %s
+                                             )LIMIT 1
+                                             '''
+
+        values_1 = (location, row.get('sub location'), row.get('sub_location_latitude'),
+                    row.get('sub_location_longitude'), row.get('name'), from_date, to_date, location,
+                    row.get('sub location'), row.get('name'), from_date, to_date)
         cur.execute(insert_query_1, values_1)
 
-        query_location_id = '''SELECT location_id
-                               FROM site_location
-                               WHERE location = %s and sub_location = %s'''
-        cur.execute(query_location_id, (location, row.get('sub location')))
-        result_location = cur.fetchone()
-        location_id = result_location["location_id"]
+        # getting the id from the location_dates table to use as foreign key in the site_info table.
+        query_id = '''SELECT id
+                      FROM location_dates
+                      WHERE location=%s and sub_location=%s and site_name=%s and from_date=%s and to_date=%s'''
+        cur.execute(query_id, (location, row.get('sub location'), row.get('name'), from_date, to_date))
+        result_id = cur.fetchone()
+        the_id = result_id["id"]
 
-        insert_query_2 = '''INSERT INTO site_info (location_id, site_name, max_people, rating, 
-                            reviewers_amount, free_cancellation, parking, breakfast, pets)
-                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+        # filling the site_info table with the option to update existing rows.
+        insert_query_2 = '''INSERT INTO site_info (location_dates_id, rating, reviewers_amount,
+                                                   free_cancellation, parking, breakfast, pets, price,
+                                                   date_time, temperature, feelslike)
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
                             ON DUPLICATE KEY UPDATE 
-                            max_people = VALUES(max_people),
                             rating = VALUES(rating),
                             reviewers_amount = VALUES(reviewers_amount),
                             free_cancellation = VALUES(free_cancellation),
                             parking = VALUES(parking),
                             breakfast = VALUES(breakfast),
-                            pets = VALUES(pets)'''
-        values_2 = (location_id, row.get('name'), row.get('Max people'), row.get('rating'), row.get('reviewers amount'),
-                    row.get('Free Cancellations'), row.get('parking'), row.get('Breakfast'), row.get('pets'))
+                            pets = VALUES(pets),
+                            price = VALUES(price),
+                            date_time = VALUES(date_time),
+                            temperature = VALUES(temperature),
+                            feelslike = VALUES(feelslike)'''
+        values_2 = (the_id, row.get('Max people'), row.get('rating'), row.get('reviewers amount'),
+                    row.get('Free Cancellations'), row.get('parking'), row.get('Breakfast'), row.get('pets'),
+                    row.get('Price'), formatted_date, row.get('temperature'), row.get('feelslike'))
         cur.execute(insert_query_2, values_2)
 
-        query_site_id = '''SELECT site_id
-                            FROM site_info
-                            WHERE site_name = %s and location_id = %s'''
-        cur.execute(query_site_id, (row.get('name'), location_id))
-        result_site = cur.fetchone()
-        site_id = result_site["site_id"]
-
-        insert_query_3 = '''INSERT INTO price_offer (location_id, site_id, price, from_date, to_date, date_time_update) 
-                            VALUES (%s, %s, %s, %s, %s, %s)
-                            ON DUPLICATE KEY UPDATE 
-                            price = %s'''
-        values_3 = (location_id, site_id, row.get('Price'), from_date, to_date, formatted_date, row.get('Price'))
-        cur.execute(insert_query_3, values_3)
-
-        insert_query_4 = '''INSERT INTO facilities (site_id, kitchen, wifi, air_conditioning) 
+        # filling the facilities table with the option to update existing rows.
+        insert_query_3 = '''INSERT INTO facilities (location_dates_id, kitchen, wifi, air_conditioning) 
                             VALUES (%s, %s, %s, %s)
                             ON DUPLICATE KEY UPDATE 
                             kitchen = VALUES(kitchen),
                             wifi = VALUES(wifi),
                             air_conditioning = VALUES(air_conditioning)'''
-        values_4 = (site_id, row.get('kitchen'), row.get('wifi'), row.get('air conditioning'))
-        cur.execute(insert_query_4, values_4)
+        values_3 = (the_id, row.get('kitchen'), row.get('wifi'), row.get('air conditioning'))
+        cur.execute(insert_query_3, values_3)
 
     connection.commit()
     csv_file.close()
     cur.close()
     connection.close()
     print("The DB was updated successfully")
-# insert_to_db("2021-10-20","2021-10-29","Germany")
+#insert_to_db("2021-11-26","2021-12-29","germany")
